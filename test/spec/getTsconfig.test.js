83977c8587be800fae2d90befa71ceeff74bce95
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-15 12:15:12 -0800

    Republish with esm compatibility

--- test/spec/getTsconfig.test.js
+++ test/spec/getTsconfig.test.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var path = require('path');
+var slash = require('slash');
 
 var get = require('../../lib');
 
@@ -12,32 +13,33 @@ describe('getTsconfig', function () {
     var cwd = process.cwd();
     process.chdir(DATA_DIR);
     var config = get.getTsconfig();
-    assert.ok(config.path.startsWith(DATA_DIR));
+
+    assert.ok(config.path.startsWith(slash(DATA_DIR)));
     assert.equal(config.config.compilerOptions.module, 'System');
     process.chdir(cwd);
   });
 
   it('filepath', function () {
     var config = get.getTsconfig(path.join(DATA_DIR, 'dummy.ts'));
-    assert.ok(config.path.startsWith(DATA_DIR));
+    assert.ok(config.path.startsWith(slash(DATA_DIR)));
     assert.equal(config.config.compilerOptions.module, 'System');
   });
 
   it('directory', function () {
     var config = get.getTsconfig(DATA_DIR);
-    assert.ok(config.path.startsWith(DATA_DIR));
+    assert.ok(config.path.startsWith(slash(DATA_DIR)));
     assert.equal(config.config.compilerOptions.module, 'System');
   });
 
   it('config path', function () {
     var config = get.getTsconfig(path.join(DATA_DIR, 'cjs', 'tsconfig.json'));
-    assert.ok(config.path.startsWith(DATA_DIR));
+    assert.ok(config.path.startsWith(slash(DATA_DIR)));
     assert.equal(config.config.compilerOptions.module, 'CommonJS');
   });
 
   it('config path extends', function () {
     var config = get.getTsconfig(path.join(DATA_DIR, 'extends', 'tsconfig.json'));
-    assert.ok(config.path.startsWith(DATA_DIR));
+    assert.ok(config.path.startsWith(slash(DATA_DIR)));
     assert.equal(config.config.compilerOptions.module, 'ESNext');
   });
 });
